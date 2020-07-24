f4ffd2c574ad761ed6d55f926e0b350a5ca0ac75
Zeyad_Shapan <nehmado@Nehmados-MacBook-Pro.local>
2020-07-24 13:59:05 +0200

    creating chat and displaying it works

--- social/views.py
+++ social/views.py
@@ -6,18 +6,18 @@ User = get_user_model()
 
 
 def create_ChatBox(request, friend_id):
-    if request.method == 'POST':
+    if request.method == 'GET':
         friend = get_object_or_404(User, pk=friend_id)
         check_chat_box = ChatBox.objects.filter(
             user_1=request.user, user_2=friend)
         check_chat_box2 = ChatBox.objects.filter(
             user_1=friend, user_2=request.user)
         if check_chat_box or check_chat_box2:
-            return redirect('social:chat_friend.html', friend_id)
+            return redirect('social:chat_friend', friend_id)
         else:
-            check_box = ChatBox(user_1=request.user, user_2=friend)
-            check_box.save
-            return redirect('social:chat_friend.html', friend_id)
+            chat_box = ChatBox(user_1=request.user, user_2=friend)
+            chat_box.save()
+            return redirect('social:chat_friend', friend_id)
 
 
 def chat_friend(request, friend_id):
