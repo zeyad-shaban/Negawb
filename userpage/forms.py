69bc0ad50a036907d4bc098fc2cc10883c2c2336
Zeyad_Shapan <nehmado@Nehmados-MacBook-Pro.local>
2020-07-26 10:45:28 +0200

    privacy option for hiding image

--- userpage/forms.py
+++ userpage/forms.py
@@ -2,6 +2,7 @@ from django.forms import ModelForm
 from django.contrib.auth import get_user_model as user_model
 User = user_model()
 
+
 class UserForm(ModelForm):
     """Update User after creating it from user page"""
 
@@ -9,11 +10,13 @@ class UserForm(ModelForm):
         """Meta definition for Userform."""
 
         model = User
-        fields = ('username', 'email', 'first_name', 'last_name', 'bio', 'avatar')
+        fields = ('username', 'email', 'first_name',
+                  'last_name', 'bio', 'avatar')
+
 
 class UserPrivacyForm(ModelForm):
     class Meta:
         model = User
-        fields = ('show_email',)
+        fields = ('show_email', 'who_see_avatar',)
 
 # username, email, first_name, last_name, date_joined and last_login, password
--- userpage/models.py
+++ userpage/models.py
@@ -4,8 +4,19 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     friends = models.ManyToManyField('User', blank=True)
-    bio = models.CharField(max_length=400, default='I love this website!', blank=True, null= True)
+    bio = models.CharField(
+        max_length=400, default='I love this website!', blank=True, null=True)
     avatar = models.ImageField(
         upload_to='profile_images', default='profile_images/DefaultUserImage.jpg')
     # PRIVACY
     show_email = models.BooleanField(default=False)
+    who_see_avatar_choices = [
+        ('none', 'No One'),
+        ('friends', 'Friends Only'),
+        ('everyone', 'Every One'),
+    ]
+    who_see_avatar = models.CharField(
+        max_length= 30,
+        choices=who_see_avatar_choices,
+        default='friends',
+    )
