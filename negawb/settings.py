179d8d27a6a2d7ce0d3cc975a6b17ab1460b8e99
Zeyad_Shapan <nehmado@Nehmados-MacBook-Pro.local>
2020-07-29 15:05:53 +0200

    Added production app

--- negawb/settings.py
+++ negawb/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'people',
     'comments',
     'social',
+    'production',
 ]
 
 MIDDLEWARE = [
---
+++ production/models.py
@@ -0,0 +1,5 @@
+from django.db import models
+
+class Timer(models.Model):
+    spend_now = models.
+    total_spend = models
