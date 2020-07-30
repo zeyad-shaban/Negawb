f242a2686c84ae191055f4bba046243acf6ee993
Zeyad_Shapan <nehmado@Nehmados-MacBook-Pro.local>
2020-07-30 14:59:41 +0200

    Ajax in, but not 100% working

--- production/views.py
+++ production/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
+from django.http import JsonResponse
+from django.forms.models import model_to_dict
 from django.contrib import messages
 from .forms import TodoForm
+
 
 def create_todo(request):
     if request.method == 'POST':
@@ -9,4 +12,4 @@ def create_todo(request):
         todo.user = request.user
         todo.save()
         messages.success(request, 'Creted Todo, Good Luck!')
-        return redirect('home')
+        return JsonResponse({'todo': model_to_dict(todo)}, status=200)
