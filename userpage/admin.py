35d0703c8edaec45ed9168a41f663925c6e0ae28
Zeyad_Shapan <nehmado@Nehmados-MacBook-Pro.local>
2020-07-30 13:55:23 +0200

    moved to modal

--- userpage/admin.py
+++ userpage/admin.py
@@ -3,4 +3,4 @@ from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth import get_user_model as user_model
 User = user_model()
 
-admin.site.register(User)
+admin.site.register(User, UserAdmin)
