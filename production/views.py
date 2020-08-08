8fb78d6cec459a27753d102041a60e04df297371
zeyadShapan <zeyadshapan2004@gmail.com>
2020-08-08 08:56:40 +0200

    maximum of 1 feedback each day

--- production/views.py
+++ production/views.py
@@ -5,11 +5,14 @@ from django.forms.models import model_to_dict
 from django.contrib import messages
 from .forms import TodoForm, FeedbackForm
 from .models import Feedback, Todo
+import datetime
+from django.utils.timezone import now
 
 
 def create_todo(request):
     if request.method == 'GET':
-        todo = Todo(title = request.GET.get('title'), note=request.GET.get('note'), is_important = request.GET.get('is_important'), user=request.user)
+        todo = Todo(title=request.GET.get('title'), note=request.GET.get(
+            'note'), is_important=request.GET.get('is_important'), user=request.user)
         todo.save()
         return JsonResponse({'todo': model_to_dict(todo)})
 
@@ -17,14 +20,22 @@ def create_todo(request):
 def feedback(request):
     if request.method == 'GET':
         feedbacks = Feedback.objects.all().order_by('-created_date')
-        return render(request, 'production/feedback.html', {'form': FeedbackForm, 'feedbacks': feedbacks })
+        return render(request, 'production/feedback.html', {'form': FeedbackForm, 'feedbacks': feedbacks})
     else:
-        form = FeedbackForm(request.POST)
-        feedback = form.save(commit=False)
-        feedback.user = request.user
-        feedback.save()
-        messages.success(request, 'Thank you for your feedback, we promise we will read it as soon as possible')
-        return redirect('production:feedback')
+        feedbacks_in_last_day = request.user.feedback_set.filter(
+            created_date__gt=now() - datetime.timedelta(days=1))
+        if feedbacks_in_last_day.count() >=1:
+            messages.error(request, 'You can make only 1 feedback each day, please wait till tommorow')
+            return redirect('home')
+        else:
+            form = FeedbackForm(request.POST)
+            feedback = form.save(commit=False)
+            feedback.user = request.user
+            feedback.save()
+            messages.success(
+                request, 'Thank you for your feedback, we promise we will read it as soon as possible')
+            return redirect('production:feedback')
+
 
 class ViewFeedback(generic.DetailView):
     model = Feedback
