ad47688f43e3961c24000a502dd4c1e64c921a9c
zeyadShapan <zeyadshapan2004@gmail.com>
2020-08-09 10:45:18 +0200

    added working pagination

--- categories/views.py
+++ categories/views.py
@@ -5,6 +5,9 @@ from comments.models import Post, Comment
 from comments.forms import PostForm
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
+# PAGINATION
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 
 
 def home(request):
@@ -13,9 +16,19 @@ def home(request):
     else:
         categories = Category.objects.all()
         all_posts = Post.objects.all().order_by('-likes')
-        return render(request, 'categories/index.html', {'categories': categories, 'all_posts':all_posts})
+        return render(request, 'categories/index.html', {'categories': categories, 'all_posts': all_posts})
+
 
 def view_category(request, pk):
     category = get_object_or_404(Category, pk=pk)
-    posts = Post.objects.filter(category=category)
+    post_list = Post.objects.filter(category=category).order_by('-post_date')
+    # Todo 2 => 7
+    paginator = Paginator(post_list, 2)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
     return render(request, f'categories/{category.title}.html', {'category': category, 'posts': posts})
