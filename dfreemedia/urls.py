19fd0f5da089bf64a055bd34e6fc401064a8dbf3
zeyadShapan <zeyadshapan2004@gmail.com>
2020-08-09 14:11:49 +0200

    Password reseting is completly working

--- dfreemedia/urls.py
+++ dfreemedia/urls.py
@@ -38,11 +38,12 @@ urlpatterns = [
     path('login/', users_views.loginuser, name='loginuser'),
     path('password-reset/', auth_views.PasswordResetView.as_view(
         template_name='users/password_reset.html'), name='password_reset'),
-
     path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(
         template_name='users/password_reset_done.html'), name='password_reset_done'),
     path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
         template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='users/password_reset_complete.html'), name='password_reset_complete'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
