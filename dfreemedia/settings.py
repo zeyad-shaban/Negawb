f3cbdf78ed97bcf7c3962a46d2b4c1d6cae7cca3
zeyadShapan <zeyadshapan2004@gmail.com>
2020-08-09 13:58:06 +0200

    Fixed email not working in settings.py

--- dfreemedia/settings.py
+++ dfreemedia/settings.py
@@ -137,7 +137,7 @@ AUTH_USER_MODEL = 'userpage.User'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Email Messaging
-EMAIL_BACKEND = 'django.core.mail.backends.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
