2388d90191302e8d04c1f2240c6d34e217f84507
zeyadShapan <zeyadshapan2004@gmail.com>
2020-08-11 12:02:24 +0200

    fixed not redirecting to login page

--- dfreemedia/settings.py
+++ dfreemedia/settings.py
@@ -132,7 +132,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-LOGIN_URL = '/user/login/'
+LOGIN_URL = '/login/'
 AUTH_USER_MODEL = 'userpage.User'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
