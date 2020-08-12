ff15f7a2655df383ec1d3bfd2d75a9a53b231b85
zeyadShapan <zeyadshapan2004@gmail.com>
2020-08-12 18:15:38 +0200

    can now display done todos

--- categories/context_processors.py
+++ categories/context_processors.py
@@ -4,13 +4,21 @@ from production.forms import TodoForm
 def add_variable_to_context(request):
     try:
         if request.user.is_authenticated:
-            todos = Todo.objects.filter(user=request.user)
+            todos = Todo.objects.filter(user=request.user, is_completed=False)
         else:
             todos = 'Please Log In'
+    except NameError:
+        todos = 'Please Log In'
+    try:
+        if request.user.is_authenticated:
+            done_todos = Todo.objects.filter(user=request.user, is_completed=True)
+        else:
+            done_todos = 'Please Log In'
     except NameError:
         todos = 'Please Log In'
 
     return {
         'todos': todos,
         'todo_form': TodoForm,
+        'done_todos': done_todos,
     }
