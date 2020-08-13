00b99a50b42976cfd2c8e955597cc9b9c5a7480c
zeyadShapan <zeyadshapan2004@gmail.com>
2020-08-13 08:07:31 +0200

    removed heroku support

--- dfreemedia/settings.py
+++ dfreemedia/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -145,9 +144,6 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
 
-
-# HEROKU
-django_heroku.settings(locals())
 
 try:
     from .local_settings import *
