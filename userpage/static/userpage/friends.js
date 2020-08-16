f4b184b490645962d905bbcf35ea4b87d7276657
zeyadShapan <zeyadshapan2004@gmail.com>
2020-08-16 20:30:18 +0200

    fixed not adding group members

--- userpage/static/userpage/friends.js
+++ userpage/static/userpage/friends.js
@@ -6,21 +6,4 @@ $(document).ready(function () {
     $('#findUserForm').submit(function (e) {
         e.preventDefault();
     })
-    $('.id_inviteForm').submit(function (e) {
-        e.preventDefault();
-        $.ajax({
-            url: $('.id_inviteForm').attr('data-url'),
-            data: {
-                'user_pk': $(this).attr('data-user_pk'),
-                'group_pk': $('#currChat').attr(
-                    'data-pk'),
-            },
-            dataType: 'json',
-            success: function (response) {
-                console.log('success')
-                
-                $('.id_inviteForm', this).fadeOut();
-            }
-        })
-    });
 });
