80c002d0db1ed563076a84364e888a7cd1d202c7
zeyadShapan <zeyadshapan2004@gmail.com>
2020-08-16 09:53:58 +0200

    working notifications for post

--- comments/views.py
+++ comments/views.py
@@ -7,6 +7,8 @@ from .forms import PostForm
 from django.db.models import Q
 from django.contrib import messages
 from django.forms.models import model_to_dict
+from social.models import Notification
+from django.urls import resolve
 # DATES
 from django.utils.timezone import now
 import datetime
@@ -18,15 +20,20 @@ def view_post(request, pk):
     comments = Comment.objects.filter(
         post=post).order_by('-comment_date')[:150]
     if request.method == 'GET' and not request.GET.get('action') == 'addComment':
-        return render(request, 'comments/view_post.html', {'post': post, 'comments': comments,})
+        return render(request, 'comments/view_post.html', {'post': post, 'comments': comments, })
     elif request.GET.get('action') == 'addComment':
         if request.GET.get('description') == '' or request.GET.get('description') == None:
             return None
         else:
-            comment = Comment(description = request.GET.get('description'), post = post, user=request.user)
+            comment = Comment(description=request.GET.get(
+                'description'), post=post, user=request.user)
             comment.save()
+            if post.user.allow_comment_message:
+                notification = Notification(notification_type='comment_message', sender=request.user, url=resolve(
+                    request.path_info).url_name, content=comment.description[:100], image=request.user.avatar)
+                notification.save()
+                notification.receiver.add(post.user)
             return JsonResponse({'comment': model_to_dict(comment)})
-
 
 
 @login_required
@@ -99,8 +106,8 @@ def create_post(request, pk):
     else:
         category = get_object_or_404(Category, pk=pk)
     posts_in_last_day = request.user.post_set.filter(Q(
-        post_date__gt=now() - datetime.timedelta(days=1)),~Q(category=fun_area_category))
-    if posts_in_last_day.count() >= 3 and category !=fun_area_category:
+        post_date__gt=now() - datetime.timedelta(days=1)), ~Q(category=fun_area_category))
+    if posts_in_last_day.count() >= 3 and category != fun_area_category:
         messages.error(
             request, f'You can only make 3 posts a day, however, you can make as many posts as you want in <a href="/category/2/" >Fun Area</a>')
 
@@ -112,7 +119,8 @@ def create_post(request, pk):
         messages.error(request, 'Please spicify at leat one field')
         return redirect('categories:view_category', pk=pk)
     elif request.FILES.get('image') != None and request.FILES.get('post_file') != None:
-        messages.error(request, 'You can\'t have both image and video in the same field')
+        messages.error(
+            request, 'You can\'t have both image and video in the same field')
         return redirect('categories:view_category', pk=pk)
     else:
         if pk != 4 and pk != 1 and pk != 6:
@@ -139,6 +147,7 @@ def create_post(request, pk):
 
 def create_reply(request, pk):
     comment = get_object_or_404(Comment, pk=pk)
-    reply = Reply(description = request.GET.get('description'), comment = comment, user = request.user)
+    reply = Reply(description=request.GET.get('description'),
+                  comment=comment, user=request.user)
     reply.save()
     return JsonResponse({'reply': model_to_dict(reply)})
