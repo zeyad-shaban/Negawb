064c33ccdfefa9894b9f0cfa9d24f31cfce0b4b5
zeyadShapan <zeyadshapan2004@gmail.com>
2020-08-17 11:53:37 +0200

    fixed deleting groups

--- static/userpage/friends.js
+++ static/userpage/friends.js
@@ -7,4 +7,34 @@ $(document).ready(function () {
         e.preventDefault();
     })
     $('nav').removeClass('sticky-top')
+    $('#addMemberSettings').hide()
+    $('.groupSettings').click(function (event) {
+        event.preventDefault();
+        $('.groupSettings').removeClass('active');
+        $(this).addClass('active');
+        if ($(this).html() == 'Add') {
+            $('#groupMembersSettings').hide()
+            $('#addMemberSettings').show()
+        } else if ($(this).html() == 'Members'){
+            $('#addMemberSettings').hide()
+            $('#groupMembersSettings').show();
+        }
+    })
+    $('#deleteGroup').click(function(evet){
+        event.preventDefault();
+        let currChat = $('#currChat').attr('data-pk')
+        let confirmation = confirm('Are you sure you want to delete the group?')
+        if (confirmation){
+            $.ajax({
+                url: $('#deleteGroup').attr('data-url'),
+                data: {
+                    'pk': $('#currChat').attr('data-pk'),
+                },
+                dataType:'json',
+                success: function(response){
+                    location.reload();
+                }
+            })
+        }
+    })
 });
