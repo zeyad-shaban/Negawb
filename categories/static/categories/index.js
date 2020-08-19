fccb638798a3f3a29f6e36696b7193f0cd90d728
zeyadShapan <zeyadshapan2004@gmail.com>
2020-08-19 11:49:55 +0200

    working ultimate scroll for images posts

--- categories/static/categories/index.js
+++ categories/static/categories/index.js
@@ -24,4 +24,74 @@ $(document).ready(function () {
             }
         }
     })
+
+    let homepage_post_page = 2
+
+    function bottomScroll() {
+        if ($(window).scrollTop() + $(window).height() == $(document).height()) {
+            $.ajax({
+                url: '',
+                data: {
+                    'homepage_posts_page': homepage_post_page
+                },
+                method: 'get',
+                dataType: 'json',
+                async: false,
+                success: function (response) {
+                    i++
+                    posts = JSON.parse(response.homepage_posts)
+                    for (post of posts) {
+                        let output = ''
+                        $.ajax({
+                            url: $('#categoryContainer').attr('data-url'),
+                            data:{
+                                'pk': post.fields.user,
+                            },
+                            method: 'get',
+                            dataType: 'json',
+                            async: false,
+                            success: function (response){
+                                let user = response.user
+                                let output = ''
+                                let userAvatar = ''
+                                if (user.who_see_avatar == 'everyone'){
+                                    userAvatar = `<img src="${ user.avatar }" alt="${ user.username }" widht="64" height="64" class="mr-3 float-left">`
+                                }
+                                else if (user.who_see_avatar == 'friends' && $('#categoryContainer').attr('data-currUser') in JSON.parse(user.friends)){
+                                    userAvatar = `<img src="${ user.avatar }" alt="${ user.username }" widht="64" height="64" class="mr-3 float-left">`
+                                }
+                                else if ($('#categoryContainer').attr('data-currUser') == user){
+                                    userAvatar = `<img src="${ user.avatar }" alt="${ user.username }" widht="64" height="64" class="mr-3 float-left">`
+                                }
+                                else {
+                                    userAvatar = `<img src="/media/profile_images/DefaultUserImage.WebP" alt="${ user.username }" widht="64" height="64" class="mr-3 float-left">`
+                                }
+                                if (post.fields.image){
+                                    output = `
+                                    <a href="/people/${user.id}">
+                                    ${userAvatar}
+    <span>${user.username}</span>
+</a>
+<div class="mb-3">
+    <div class="card-body">
+        <p class="card-text">${post.fields.description}</p>
+        <img src="/media/${ post.fields.image }" alt="${ user.username }" height="250" width="40%">
+        <p class="card-text"><small class="form-text text-muted">Published at ${ post.fields.post_date}</small>
+        </p>
+    </div>
+</div>
+                                    `
+                                }
+                                $('#postsContainer').append(output)
+                            }
+                        })
+                    }
+                }
+            })
+        }
+    }
+    bottomScroll();
+    $(window).scroll((e) => {
+        bottomScroll()
+    })
 })
--- categories/views.py
+++ categories/views.py
@@ -1,47 +1,61 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, redirect, get_object_or_404
-from django.http import HttpResponse
+from django.http import JsonResponse, HttpResponse
 from .models import Category
 from comments.models import Post, Comment
 from comments.forms import PostForm
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
+from django.core.serializers import serialize
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
 def home(request):
-    if request.GET.get('q'):
-        return redirect('results')
-    else:
-        categories = Category.objects.all()
-        if request.user.is_authenticated:
-            friends = User.objects.filter(friends=request.user)
-            followed = User.objects.filter(
-                Q(followers=request.user), ~Q(friends=request.user))
-            friends_posts_list = Post.objects.filter(
-                Q(user__in=friends), ~Q(user=request.user)).order_by('-post_date')
-            followed_posts_list = Post.objects.filter(
-                Q(user__in=followed), ~Q(user=request.user)).order_by('-post_date')
-            friends_paginator = Paginator(friends_posts_list, 7)
-            followed_paginator = Paginator(followed_posts_list, 7)
-            page = request.GET.get('page')
+    categories = Category.objects.all()
+    if request.user.is_authenticated:
+        friends = User.objects.filter(friends=request.user)
+        followed = User.objects.filter(
+            Q(followers=request.user), ~Q(friends=request.user))
+        friends_posts_list = Post.objects.filter(
+            Q(user__in=friends), ~Q(user=request.user)).order_by('-post_date')
+        followed_posts_list = Post.objects.filter(
+            Q(user__in=followed), ~Q(user=request.user)).order_by('-post_date')
+        friends_paginator = Paginator(friends_posts_list, 7)
+        followed_paginator = Paginator(followed_posts_list, 7)
+        page = request.GET.get('page')
+        try:
+            friends_posts = friends_paginator.page(page)
+            followed_posts = followed_paginator.page(page)
+        except PageNotAnInteger:
+            friends_posts = friends_paginator.page(1)
+            followed_posts = followed_paginator.page(1)
+        except EmptyPage:
+            friends_posts = friends_paginator.page(
+                friends_paginator.num_pages)
+            followed_posts = followed_paginator.page(
+                followed_paginator.num_pages)
+        # Posts Paginator
+        if request.user.homepage_posts:
+            homepage_posts_list = request.user.homepage_posts.post_set.all().order_by('-post_date')
+            homepage_posts_paginator = Paginator(homepage_posts_list, 5)
+            page = request.GET.get('homepage_posts_page')
             try:
-                friends_posts = friends_paginator.page(page)
-                followed_posts = followed_paginator.page(page)
+                homepage_posts = homepage_posts_paginator.page(page)
             except PageNotAnInteger:
-                friends_posts = friends_paginator.page(1)
-                followed_posts = followed_paginator.page(1)
+                homepage_posts = homepage_posts_paginator.page(1)
             except EmptyPage:
-                friends_posts = friends_paginator.page(
-                    friends_paginator.num_pages)
-                followed_posts = followed_paginator.page(
-                    followed_paginator.num_pages)
+                homepage_posts = []
+            if page:
+                return JsonResponse({'homepage_posts':serialize('json', homepage_posts)})
         else:
-            friends_posts = None
-            followed_posts = None
+            homepage_posts = []
+    else:
+        friends_posts = None
+        followed_posts = None
+        homepage_posts = []
 
-        return render(request, 'categories/index.html', {'categories': categories, 'friends_posts': friends_posts, 'followed_posts': followed_posts, })
+    return render(request, 'categories/index.html', {'categories': categories, 'friends_posts': friends_posts, 'followed_posts': followed_posts, 'homepage_posts': homepage_posts })
 
 
 def view_category(request, pk):
