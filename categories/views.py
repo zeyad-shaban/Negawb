47f969832d4a4f45efaf1ebc9a58df8ef06042f4
zeyadShapan <zeyadshapan2004@gmail.com>
2020-08-24 10:15:51 +0200

    posts showing if user is not authenticated

--- categories/views.py
+++ categories/views.py
@@ -17,6 +17,7 @@ def home(request):
         messages.warning(request, 'You have enabled Chat only mode, you can disable it from profile Distraction Free settings')
         return redirect('chat')
     categories = Category.objects.all()
+    
     followed_users = []
     if request.user.is_authenticated:
         friends = User.objects.filter(friends=request.user)
@@ -66,8 +67,9 @@ def home(request):
     else:
         friends_posts = None
         followed_posts = None
-        homepage_posts = []
-
+        homepage_posts = Category.objects.get(title = 'Fun Area').post_set.all().order_by('-post_date')
+    print(homepage_posts)
+    print('hallo???????')
     return render(request, 'categories/index.html', {'categories': categories, 'friends_posts': friends_posts, 'followed_posts': followed_posts, 'homepage_posts': homepage_posts, 'followed_users': followed_users})
 
 
