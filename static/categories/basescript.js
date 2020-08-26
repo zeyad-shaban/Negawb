c12e6ac0c21fb42aaef7416a950e3705ed895eb7
zeyadShapan <zeyadshapan2004@gmail.com>
2020-08-26 09:16:23 +0200

    added User avatar frame for first 1000 users

--- static/categories/basescript.js
+++ static/categories/basescript.js
@@ -1,101 +1,38 @@
-$(document).ready(function () {
-    function displayNav() {
-        if ($(window).width() < 562){
-            $('#Userusername').html('')
-        }
-        if ($(window).width() > 562){
-            $('#Userusername').html($('#Userusername').attr('data-username'))
-        }
-        if ($(window).width() < 526){
-            $('#findFriends').html('<i class="fas fa-user-plus" style="font-size: 36px;"></i>')
-        }
-        if ($(window).width() > 526){
-            $('#findFriends').html('Find Friends')
-        }
-    }
-    displayNav();
-    $(window).resize(function (e){
-        displayNav();
-    })
-    $('.todoNote').css('display', 'none')
-    $('.todoItem').on('click', function (event) {
-        // $('.todoNote').toggle()
-        $('.todoNote', this).toggle()
-    })
-
-    // * Load notifications
-    setInterval(() => {
-        let currNotificationsCount
-        if ($('#currNotificationsCount').html()) {
-            currNotificationsCount = $('#currNotificationsCount').html()
-        } else {
-            currNotificationsCount = 0
-        }
-        let wantedType = 'all'
-        $.ajax({
-            url: $('#notificationsList').attr('data-url'),
-            data: {
-                'notification_type': wantedType,
-            },
-            dataType: 'json',
-            method: 'get',
-            success: function (response) {
-                let notifications = JSON.parse(response.notifications)
-                if (notifications.length > currNotificationsCount) {
-                    $('#notificationsContainer').html('')
-                    $('#currNotificationsCount').html(notifications.length)
-                    for (let i = 0; notifications.length > i; i++) {
-                        $.ajax({
-                            url: $('#notificationsList').attr('data-getUserUrl'),
-                            data: {
-                                'pk': notifications[i].fields.sender,
-                            },
-                            dataType: 'json',
-                            async: false,
-                            success: function (response) {
-                                output = ''
-                                let sender = response.user
-                                sender_friends = JSON.parse(sender.friends)
-                                let currUser = $('#notificationsList').attr('data-currUser')
-                                let senderAvatar
-                                if (sender.who_see_avatar == 'everyone') {
-                                    senderAvatar = sender.avatar
-                                    // } else if (sender.who_see_avatar == 'friends' && currUser in sender_friends) {
-                                } else {
-                                    senderAvatar = '/media/profile_images/DefaultUserImage.jpg'
-                                }
-                                output = `
-                                <a href="${notifications[i].fields.url}" style="text-decoration: none; color: black">
-                                    <div class="media" style="background: #F7F7F7;">
-                                        <img src="${sender.avatar}" alt="User Image" width="50"
-                                            height="50">
-                                        <div class="media-body">
-                                            <h5 class="mt-0">${ sender.username }</h5>
-                                            <p>${ notifications[i].fields.content }</p>
-                                        </div>
-                                    </div>
-                                </a>
-                                <hr>
-                                `
-                                $('#notificationsContainer').append(output)
-                            }
-                        })
-                    }
-                    document.getElementById('notificationSound').play()
-                }
+document.addEventListener('DOMContentLoaded', function () {
+    window.addEventListener('load', function () {
+        function displayNav() {
+            if ($(window).width() < 562) {
+                $('#Userusername').html('')
             }
+            if ($(window).width() > 562) {
+                $('#Userusername').html($('#Userusername').attr('data-username'))
+            }
+            if ($(window).width() < 526) {
+                $('#findFriends').html('<i class="fas fa-user-plus" style="font-size: 36px;"></i>')
+            }
+            if ($(window).width() > 526) {
+                $('#findFriends').html('Find Friends')
+            }
+        }
+        displayNav();
+        $(window).resize(function (e) {
+            displayNav();
         })
-    }, 6000);
-    $('#notificationDropMenu').click(function (event) {
-        event.stopPropagation();
-    });
-    $('.notificationType').click(function (e) {
-        e.preventDefault();
-        if (!$(this).hasClass('active')) {
-            $('#notificationsContainer').html('')
-            $('.notificationType').removeClass('active');
-            $(this).addClass('active')
-            let wantedType = $(this).attr('date-wantedType')
+        $('.todoNote').css('display', 'none')
+        $('.todoItem').on('click', function (event) {
+            // $('.todoNote').toggle()
+            $('.todoNote', this).toggle()
+        })
+
+        // * Load notifications
+        setInterval(() => {
+            let currNotificationsCount
+            if ($('#currNotificationsCount').html()) {
+                currNotificationsCount = $('#currNotificationsCount').html()
+            } else {
+                currNotificationsCount = 0
+            }
+            let wantedType = 'all'
             $.ajax({
                 url: $('#notificationsList').attr('data-url'),
                 data: {
@@ -105,27 +42,30 @@ $(document).ready(function () {
                 method: 'get',
                 success: function (response) {
                     let notifications = JSON.parse(response.notifications)
-                    for (let i = 0; notifications.length > i; i++) {
-                        $.ajax({
-                            url: $('#notificationsList').attr('data-getUserUrl'),
-                            data: {
-                                'pk': notifications[i].fields.sender,
-                            },
-                            dataType: 'json',
-                            async: false,
-                            success: function (response) {
-                                output = ''
-                                let sender = response.user
-                                sender_friends = JSON.parse(sender.friends)
-                                let currUser = $('#notificationsList').attr('data-currUser')
-                                let senderAvatar
-                                if (sender.who_see_avatar == 'everyone') {
-                                    senderAvatar = sender.avatar
-                                    // } else if (sender.who_see_avatar == 'friends' && currUser in sender_friends) {
-                                } else {
-                                    senderAvatar = '/media/profile_images/DefaultUserImage.jpg'
-                                }
-                                output = `
+                    if (notifications.length > currNotificationsCount) {
+                        $('#notificationsContainer').html('')
+                        $('#currNotificationsCount').html(notifications.length)
+                        for (let i = 0; notifications.length > i; i++) {
+                            $.ajax({
+                                url: $('#notificationsList').attr('data-getUserUrl'),
+                                data: {
+                                    'pk': notifications[i].fields.sender,
+                                },
+                                dataType: 'json',
+                                async: false,
+                                success: function (response) {
+                                    output = ''
+                                    let sender = response.user
+                                    sender_friends = JSON.parse(sender.friends)
+                                    let currUser = $('#notificationsList').attr('data-currUser')
+                                    let senderAvatar
+                                    if (sender.who_see_avatar == 'everyone') {
+                                        senderAvatar = sender.avatar
+                                        // } else if (sender.who_see_avatar == 'friends' && currUser in sender_friends) {
+                                    } else {
+                                        senderAvatar = '/media/profile_images/DefaultUserImage.jpg'
+                                    }
+                                    output = `
                                 <a href="${notifications[i].fields.url}" style="text-decoration: none; color: black">
                                     <div class="media" style="background: #F7F7F7;">
                                         <img src="${sender.avatar}" alt="User Image" width="50"
@@ -138,19 +78,84 @@ $(document).ready(function () {
                                 </a>
                                 <hr>
                                 `
-                                $('#notificationsContainer').append(output)
-                            }
-                        })
+                                    $('#notificationsContainer').append(output)
+                                }
+                            })
+                        }
+                        document.getElementById('notificationSound').play()
                     }
                 }
             })
-        }
+        }, 6000);
+        $('#notificationDropMenu').click(function (event) {
+            event.stopPropagation();
+        });
+        $('.notificationType').click(function (e) {
+            e.preventDefault();
+            if (!$(this).hasClass('active')) {
+                $('#notificationsContainer').html('')
+                $('.notificationType').removeClass('active');
+                $(this).addClass('active')
+                let wantedType = $(this).attr('date-wantedType')
+                $.ajax({
+                    url: $('#notificationsList').attr('data-url'),
+                    data: {
+                        'notification_type': wantedType,
+                    },
+                    dataType: 'json',
+                    method: 'get',
+                    success: function (response) {
+                        let notifications = JSON.parse(response.notifications)
+                        for (let i = 0; notifications.length > i; i++) {
+                            $.ajax({
+                                url: $('#notificationsList').attr('data-getUserUrl'),
+                                data: {
+                                    'pk': notifications[i].fields.sender,
+                                },
+                                dataType: 'json',
+                                async: false,
+                                success: function (response) {
+                                    output = ''
+                                    let sender = response.user
+                                    sender_friends = JSON.parse(sender.friends)
+                                    let currUser = $('#notificationsList').attr('data-currUser')
+                                    let senderAvatar
+                                    if (sender.who_see_avatar == 'everyone') {
+                                        senderAvatar = sender.avatar
+                                        // } else if (sender.who_see_avatar == 'friends' && currUser in sender_friends) {
+                                    } else {
+                                        senderAvatar = '/media/profile_images/DefaultUserImage.jpg'
+                                    }
+                                    output = `
+                                <a href="${notifications[i].fields.url}" style="text-decoration: none; color: black">
+                                    <div class="media" style="background: #F7F7F7;">
+                                        <img src="${sender.avatar}" alt="User Image" width="50"
+                                            height="50">
+                                        <div class="media-body">
+                                            <h5 class="mt-0">${ sender.username }</h5>
+                                            <p>${ notifications[i].fields.content }</p>
+                                        </div>
+                                    </div>
+                                </a>
+                                <hr>
+                                `
+                                    $('#notificationsContainer').append(output)
+                                }
+                            })
+                        }
+                    }
+                })
+            }
+        });
+        setInterval(() => {
+            if ($('#message_area').html()) {
+                setTimeout(() => {
+                    $('#message_area').html('')
+                }, 4000);
+            }
+        }, 4000);
     });
-    setInterval(() => {
-        if ($('#message_area').html()) {
-            setTimeout(() => {
-                $('#message_area').html('')
-            }, 4000);
-        }
-    }, 4000);
+    $('textarea,input[type=text]').emojioneArea({
+        pickerPosition: "bottom",
+    })
 });
