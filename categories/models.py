2280a332944a7a923477ca78d4d5d8e33a8020cd
zeyadShapan <zeyadshapan2004@gmail.com>
2020-08-26 14:09:47 +0200

    Complete change in categories looking

--- categories/models.py
+++ categories/models.py
@@ -3,22 +3,21 @@ from PIL import Image
 
 
 class Category(models.Model):
-    title = models.CharField(max_length=30)
-    description = models.CharField(max_length=150, null=True, blank=True)
-    image = models.ImageField(upload_to='categories_images')
-
+    title = models.CharField(max_length=25)
+    description = models.CharField(max_length=120, null=True, blank=True)
+    display_choices = [
+        ('primary', 'primary'),
+        ('secondary', 'secondary'),
+        ('success', 'success'),
+        ('danger', 'danger'),
+        ('warning', 'warning'),
+        ('info', 'info'),
+        ('light', 'light'),
+        ('dark', 'dark'),
+    ]
+    display = models.CharField(max_length= 30, choices=display_choices, default="dark")
     def __str__(self):
         return self.title
-
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
-        if self.image:
-            img = Image.open(self.image.path)
-            if img.width > 348 or img.height > 217:
-                output_size = (348, 217)
-                img.thumbnail(output_size)
-                img.save(self.image.path, 'WebP')
-
 
 
 
