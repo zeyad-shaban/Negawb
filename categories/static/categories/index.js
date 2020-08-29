0298f1c4270ccc82b959a6a71a196a087db28481
zeyadShapan <zeyadshapan2004@gmail.com>
2020-08-29 13:45:04 +0200

    separated likes in homepage_posts and fixed weird displaying

--- categories/static/categories/index.js
+++ categories/static/categories/index.js
@@ -273,5 +273,37 @@ document.addEventListener('DOMContentLoaded', function () {
         $(window).scroll((e) => {
             bottomScroll()
         })
+        // Like
+        $('.likeForm').submit(function (e) {
+            e.preventDefault();
+            let thisElement = $(this)
+            $.ajax({
+                url: thisElement.attr('action'),
+                data: {
+                    'submit': 'like',
+                },
+                dataType: 'json',
+                method: 'get',
+                async: false, // Have a look into this
+                success: function (response) {
+                    let likes = $(`#id_likes${thisElement.attr('data-pk')}`).find('p').html()
+                    let dislikes = $(`#id_dislikes${thisElement.attr('data-pk')}`).find('p').html()
+                    if (response.action == 'undislike_and_like') {
+                        dislikes -= 1
+                        likes++
+                        $(`#id_dislikes${thisElement.attr('data-pk')}`).html(`<p style="color:black;">${dislikes}</p>`)
+                        $(`#id_likes${thisElement.attr('data-pk')}`).html(`<p style="color:#065FD4;">${likes}</p>`)
+                    } else if (response.action == 'unlike') {
+                        likes -= 1
+                        $(`#id_likes${thisElement.attr('data-pk')}`).html(`<p style="color:black;">${likes}</p>`)
+                    } else { //Like only
+                        likes++
+                        $(`#id_likes${thisElement.attr('data-pk')}`).html(`<p style="color:#065FD4;">${likes}</p>`)
+                    }
+                }
+            })
+        })
+        // Dislike
+        
     })
 })
