2b927f4001b17dc91dca2c8780b1f516a24d0ce8
zeyadShapan <zeyadshapan2004@gmail.com>
2020-09-11 12:29:11 +0200

    Better displaying delete  and add areas

--- social/models.py
+++ social/models.py
@@ -86,8 +86,8 @@ class GroupRequest(models.Model):
 
 class Area(models.Model):
     name = models.CharField(max_length=20)
-    mute = models.BooleanField(default=False)
     group = models.ForeignKey(ChatGroup, on_delete=models.CASCADE)
+    muted_users = models.ManyToManyField(User, blank=True, null=True)
 
     def __str__(self):
         return self.name
