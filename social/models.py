8147906c57bd758d74ce3f09492010d637efa395
zeyadShapan <zeyadshapan2004@gmail.com>
2020-09-11 13:24:27 +0200

    Removed null

--- social/models.py
+++ social/models.py
@@ -87,7 +87,7 @@ class GroupRequest(models.Model):
 class Area(models.Model):
     name = models.CharField(max_length=20)
     group = models.ForeignKey(ChatGroup, on_delete=models.CASCADE)
-    muted_users = models.ManyToManyField(User, blank=True, null=True)
+    muted_users = models.ManyToManyField(User, blank=True)
 
     def __str__(self):
         return self.name
