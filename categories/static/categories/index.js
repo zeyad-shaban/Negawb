1545e44fdc161f65e4526fa238a7a6ea7370cdf2
zeyadShapan <zeyadshapan2004@gmail.com>
2020-09-14 08:29:44 +0200

    Added code to show add to homescreen button

--- categories/static/categories/index.js
+++ categories/static/categories/index.js
@@ -417,5 +417,40 @@ document.addEventListener('DOMContentLoaded', function () {
                 $("#uploadedImage").append('<br>');
             }
         })
+
+
+
+        // Add to home screen
+        let deferredPrompt;
+        const addBtn = document.querySelector('#addToHomeScreen');
+        addBtn.style.display = 'none'
+
+        window.addEventListener('beforeinstallprompt', (e) => {
+            // Prevent Chrome 67 and earlier from automatically showing the prompt
+            e.preventDefault();
+            // Stash the event so it can be triggered later.
+            deferredPrompt = e;
+            // Update UI to notify the user they can add to home screen
+            addBtn.style.display = 'block';
+
+            addBtn.addEventListener('click', (e) => {
+                // hide our user interface that shows our A2HS button
+                addBtn.style.display = 'none';
+                // Show the prompt
+                deferredPrompt.prompt();
+                // Wait for the user to respond to the prompt
+                deferredPrompt.userChoice.then((choiceResult) => {
+                    if (choiceResult.outcome === 'accepted') {
+                        console.log('User accepted the A2HS prompt');
+                    } else {
+                        console.log('User dismissed the A2HS prompt');
+                    }
+                    deferredPrompt = null;
+                });
+            });
+        });
+        // End add to home screen
+
+
     })
 })
